25f52b7017988f8d41a4bac5b18a9f2891223353
jayeshjain98 <48246847+jayeshjain98@users.noreply.github.com>
2021-05-20 22:53:20 +0530

    Update manage.py

--- mysite/manage.py
+++ mysite/manage.py
@@ -7,6 +7,7 @@ import sys
 """ this is a test comment for webhook testing """
 """ this is a test comment for webhook testing """
 """ this is a test comment for webhook testing """
+""" this is a test comment for webhook testing """
 
 
 def main():
