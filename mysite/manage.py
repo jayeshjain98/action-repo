9110f8389d863d99be579104e4c51e399648d516
Jayesh Jain <48246847+jayeshjain98@users.noreply.github.com>
2021-05-24 11:25:26 +0530

    Update manage.py

--- mysite/manage.py
+++ mysite/manage.py
@@ -5,7 +5,6 @@ import sys
 """ this is a test comment for webhook testing """
 """ this is a test comment for webhook testing """
 """ this is a test comment for webhook testing """
-""" this is a test comment for webhook testing """
 
 def main():
     """Run administrative tasks."""
